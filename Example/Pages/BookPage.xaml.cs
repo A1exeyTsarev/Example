22a46be262cdbf572d990cf55d78f97dadfcbe6d
Дмитрий <@TSAREVD-PC>
2026-09-14 22:42:03 +0300

    Добавьте файлы проекта.

---
+++ Example/Pages/BookPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Example.AppData;
+using Example.Pages;
+
+namespace Example.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для BookPage.xaml
+    /// </summary>
+    public partial class BookPage : Page
+    {
+        public BookPage()
+        {
+            InitializeComponent();
+            loadBooks();
+            checkRole();
+
+        }
+        private void loadBooks()
+        {
+            try
+            {
+                var Books = AppConnect.model1.Books.Select(b => new
+                {
+                    book_id = b.Book_Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Year = b.Year,
+                    Price = b.Price,
+
+                }).ToList();
+                LvBooks.ItemsSource = Books;
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки: {ex.Message}");
+            }
+        }
+        private void checkRole()
+        {
+            if(LoginPage.CurrentRoleName=="Manager")
+            {
+                BtnAdd.Visibility=Visibility.Visible;
+                BtnDelete.Visibility=Visibility.Visible;
+                BtnEdit.Visibility=Visibility.Visible;
+
+            }
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new AddPage());
+        }
+        private void BtnEdite_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new EditePage());
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new DeletePage());
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new LoginPage());
+        }
+    }
+}
---
+++ Example/Pages/LoginPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Example.AppData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+            public static int CurrentUserId;
+            public static string CurrentRoleName;
+
+        private void ButLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(LoginBox.Text) || string.IsNullOrEmpty(PasswordBox.Password))
+                {
+                    MessageBox.Show("Введите логин и пароль", "Ошибка", 
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                var user =AppConnect.model1.Users.FirstOrDefault(x => x.Login == LoginBox.Text && x.Password==PasswordBox.Password);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Неверный логин или пароль", "Ошибка", 
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                CurrentUserId = user.User_Id;
+                CurrentRoleName = user.Roles.RoleName;
+
+                MessageBox.Show($"Добро пожаловать, {user.UserFullName}! "+$"Роль, {user.Roles.RoleName}", "Успех", 
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+
+                AppFrame.MainFrame.Navigate(new BookPage());
+            }
+            catch (Exception ex) 
+            
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
